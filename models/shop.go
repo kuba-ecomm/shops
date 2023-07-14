c660a756f0a0ae09d8d2a263d59c3f5d20533977
kuba-ecomm <139096149+kuba-ecomm@users.noreply.github.com>
2023-07-14 06:29:15 +0200

    Update shop.go

--- models/shop.go
+++ models/shop.go
@@ -12,6 +12,25 @@ type Shop struct {
 	Name        string
 	Description string
 	Code        string
+	Stock       *Stock
+}
+
+type Stock struct {
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Source      string  `json:"source"`
+	Price       float64 `json:"price"`
+	StockType   string  `json:"stockType"` // todo: change to StockType iota
+}
+
+func NewStock(title, description, source, stockType string, price float64) *Stock {
+	return &Stock{
+		Title:       title,
+		Description: description,
+		Source:      source,
+		Price:       price,
+		StockType:   stockType,
+	}
 }
 
 // Proto is
