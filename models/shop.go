69235b02a7154ef699fd2894d5db838a42b96c58
kuba-ecomm <139096149+kuba-ecomm@users.noreply.github.com>
2023-07-18 00:11:33 +0200

    Add files via upload

---
+++ models/shop.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"github.com/gofrs/uuid"
+
+	proto "github.com/kuba-ecomm/shops/protocols/shops"
+)
+
+// Shop is
+type Shop struct {
+	UUID        uuid.UUID
+	Name        string
+	Description string
+	Code        string
+	Stock       *Stock
+}
+
+// Proto is
+func (b Shop) Proto() *proto.Shop {
+	shop := &proto.Shop{
+		Uuid:        b.UUID.Bytes(),
+		Name:        b.Name,
+		Description: b.Description,
+		Code:        b.Code,
+	}
+
+	return shop
+}
+
+// ShopFromProto is
+func ShopFromProto(pb *proto.Shop) *Shop {
+	shop := &Shop{
+		UUID:        uuid.FromBytesOrNil(pb.Uuid),
+		Name:        pb.Name,
+		Description: pb.Description,
+		Code:        pb.Code,
+	}
+
+	return shop
+}
+
+// ShopsToProto is
+func ShopsToProto(battles []*Shop) (pb []*proto.Shop) {
+	for _, b := range battles {
+		pb = append(pb, b.Proto())
+	}
+	return
+}
+
+// ShopsFromProto is
+func ShopsFromProto(pb []*proto.Shop) (shops []*Shop) {
+	for _, b := range pb {
+		shops = append(shops, ShopFromProto(b))
+	}
+	return
+}
---
+++ models/stock.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"github.com/gofrs/uuid"
+	proto "github.com/kuba-ecomm/shops/protocols/shops"
+)
+
+type Stock struct {
+	UUID        uuid.UUID
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Source      string  `json:"source"`
+	Brand       string  `json:"brand"`
+	Price       float64 `json:"price"`
+	StockType   string  `json:"stockType"` // todo: change to StockType iota
+}
+
+func NewStock(title, description, source, stockType, brand string, price float64, uuid uuid.UUID) *Stock {
+	return &Stock{
+		UUID:        uuid,
+		Title:       title,
+		Description: description,
+		Source:      source,
+		Price:       price,
+		StockType:   stockType,
+		Brand:       brand,
+	}
+}
+
+func (st Stock) ToProto() *proto.Stock {
+	return &proto.Stock{
+		Title:       st.Title,
+		Description: st.Description,
+		Source:      st.Source,
+		StockType:   st.StockType,
+		Price:       float32(st.Price),
+		Brand:       st.Brand,
+		Uuid:        st.UUID.Bytes(),
+	}
+}
+
+func StockFromProto(pb *proto.Stock) *Stock {
+	shop := &Stock{
+		UUID:        uuid.FromBytesOrNil(pb.Uuid),
+		Title:       pb.Title,
+		Description: pb.Description,
+		Source:      pb.Source,
+		StockType:   pb.StockType,
+		Price:       float64(pb.Price),
+		Brand:       pb.Brand,
+	}
+
+	return shop
+}
